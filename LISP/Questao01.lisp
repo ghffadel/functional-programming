4dc513917dc3173ea3968f970637c9ae3d2e47dd
ghffadel <ghff.snf20@uea.edu.br>
2022-03-10 14:56:57 -0400

    Added comments in 1st LISP exercise

--- LISP/Questao01.lisp
+++ LISP/Questao01.lisp
@@ -1,8 +1,13 @@
+; Função de fatorial
 (defun fatorial (numero)
+    ; Retona 1 se o número é igual a 0
     (cond ((zerop numero) 1)
+        ; fatorial(numero) = numero * fatorial(numero - 1)
         (t ( * numero (fatorial (- numero 1))))
     )
 )
 
+; Lê o número
 (setq numero (read))
+; Imprime o resultado
 (format t "Fatorial de ~d é: ~d" numero (fatorial numero))
