ab19eb04bd700e4554829196fdc6757c316e00e7
ghffadel <ghff.snf20@uea.edu.br>
2022-03-15 09:03:59 -0400

    Added 4th LISP exercise

---
+++ LISP/Questao04.lisp
@@ -0,0 +1,13 @@
+; Definição da função de união
+(defun uniao (lista1 lista2)
+  (if (endp lista1)
+      lista2
+      (uniao (rest lista1)
+        (adjoin (first lista1)
+            lista2))))
+
+; Lê as duas listas
+(setq lista1 (read))
+(setq lista2 (read))
+; Imprime a união
+(format t "~a" (uniao lista1 lista2))
