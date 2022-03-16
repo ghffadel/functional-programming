ff49694715dd4fec2e5b9276bd8445ca12c78807
ghffadel <ghff.snf20@uea.edu.br>
2022-03-16 08:05:23 -0400

    Added 8th Erlang exercise

---
+++ Erlang/Questao08.erl
@@ -0,0 +1,14 @@
+% Define o módulo do arquivo
+-module(Questao08).
+
+% Funções que serão exportadas
+-export([potencia/2]).
+
+% Definição da função potência
+potencia(lista) ->
+    % Tamanho da lista
+    tamanho = length(lista)
+    % Quantidade de subconjuntos
+    maximo = trunc(math:pow(2, tamanho))
+    % Gera todas as possibilidades de subconjuntos usando bitwise
+    [[lists:nth(posicao + 1, lista) || posicao <- lists:seq(0, tamanho - 1), I band (1 bsl posicao) =/= 0 || I <- lists:seq(0, maximo - 1)].
