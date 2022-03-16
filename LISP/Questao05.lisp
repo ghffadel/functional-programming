7151657d661955ebe92a1d908058b0ef73b3f7e2
ghffadel <ghff.snf20@uea.edu.br>
2022-03-16 14:07:56 -0400

    Added 5th LISP exercise

---
+++ LISP/Questao05.lisp
@@ -0,0 +1,15 @@
+; Definição da função de intersecção
+(defun interseccao (lista1 lista2)
+    (cond ((null lista1) nil)
+        ((member (car lista1) lista2)
+        (cons (car lista1) (interseccao (cdr lista1) lista2)))
+        (t (interseccao (cdr lista1) lista2))
+    )
+)
+
+
+; Lê as duas listas no formato (a, b, c, ...)
+(setq lista1 (read))
+(setq lista2 (read))
+; Imprime a união
+(format t "~a" (interseccao lista1 lista2))
